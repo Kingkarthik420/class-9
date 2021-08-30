0bd110f3d2c8b411a103435c1a4fe3456f2d1af6
Kingkarthik420 <89799301+Kingkarthik420@users.noreply.github.com>
2021-08-30 13:12:31 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+
+function setup() {
+  createCanvas(400,400);
+   var fire=createSprite(200,200,10,80)
+   
+   fire.velocityX=3
+}
+
+function draw() 
+{
+  background("blue");
+drawSprites()
+}
+
+
+
+
